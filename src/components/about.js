e81a2740dde7abefd426ad8771c58e094bd15ccc
ravigairola <ravigairola248@gmail.com>
2023-05-22 17:10:52 +0530

    code modified agsin

--- src/components/about.js
+++ src/components/about.js
@@ -16,7 +16,7 @@ return(
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae error sit ab culpa necessitatibus id
         enim porro. Placeat autem, similique fugit quas unde ipsam assumenda deserunt esse. Iure quibusdam quae,
         esse consectetur ut ullam ab, impedit quidem atque nihil voluptas.</p>
-    <a href="#" class="btn">Read More</a>
+    <a href="/" class="btn">Read More</a>
 </div>
 </section>
 <section class="services" id="services">
@@ -30,21 +30,21 @@ return(
         <h3>Web Developer</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui placeat nihil suscipit velit alias
             magnam tempore maiores? Quo soluta perspiciatis quod facilis.</p>
-        <a href="" class="btn">Read More</a>
+        <a href="/" class="btn">Read More</a>
     </div>
     <div class="services-box">
         <i class='bx bx-bar-chart-alt'></i>
         <h3>Digital Marketing</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui placeat nihil suscipit velit alias
             magnam tempore maiores? Quo soluta perspiciatis quod facilis.</p>
-        <a href="" class="btn">Read More</a>
+        <a href="/" class="btn">Read More</a>
     </div>
     <div class="services-box">
         <i class='bx bxs-paint'></i>
         <h3>Graphic Desiner </h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui placeat nihil suscipit velit alias
             magnam tempore maiores? Quo soluta perspiciatis quod facilis.</p>
-        <a href="" class="btn">Read More</a>
+        <a href="/" class="btn">Read More</a>
     </div>
 </div>
 </section>
